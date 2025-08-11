f959675bb08329c8077a7676819efb2cbd79080e
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-11 11:36:21 -0300

    commit

---
+++ my-project/src/components/ui/command.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { type DialogProps } from "@radix-ui/react-dialog"
+import { Command as CommandPrimitive  } from "cmdk"
+import { Search } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Dialog, DialogContent } from "@/components/ui/dialog"
+
+const Command = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive> 
+>(({ className, ...Props}, ref) =>  (
+    <CommandPrimitive
+        ref={ref}
+        className={cn (
+            "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover",
+             className
+        )}
+        {...props}
+        />
+
+))
+Command.displayName = CommandPrimitive.displayName
+  
+interface  CommandDialogProps extends DialogProps {}
+
+const CommandDialogProps = ({ children, ...props}: CommandDialogProps) => {
+    return (
+        <Dialog {...props}>
+          <DialogContent className="overflow-hidden p-0 shadow-lg">
+            <Command>
+                {children}
+
+    
+    
+}
