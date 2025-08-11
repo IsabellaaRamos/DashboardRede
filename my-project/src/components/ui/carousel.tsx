0f4a923acb93257ae5d9b9e6c59ba2b3cd0e7037
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-11 09:18:48 -0300

    adiciona propriedades opcionais ao tipo CarouselProps

--- my-project/src/components/ui/carousel.tsx
+++ my-project/src/components/ui/carousel.tsx
@@ -14,4 +14,9 @@ type CarouselOptions = UseCArouselPametrs[0]
 type CarouselPlugin = UseCArouselPametrs[1]
 
 type CarouselProps = {
-    
+    opts?: CarouselOptions
+    plugins?: CarouselPlugin
+    orientation?: "horizontal" | "vertical"
+    setApi?: (api: CarouselApi) => void
+
+}
