c87220ecd1f96c7d51c605eb0082e65dce9a6693
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-11 09:17:05 -0300

    adiciona o componente Carousel com estrutura inicial

---
+++ my-project/src/components/ui/carousel.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import useEmblaCarousel, {
+    type EmblaCarouselType,
+} from "embla-carousel-react"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+
+type CarouselApi = useEmblaCarousel[1]
+type UseCArouselPametrs = Parameters<typeof useEmblaCarousel>
+type CarouselOptions = UseCArouselPametrs[0]
+type CarouselPlugin = UseCArouselPametrs[1]
+
+type CarouselProps = {
+    
