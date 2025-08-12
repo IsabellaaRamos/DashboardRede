347bec29c8d060e3deaf4f75163988d22ff237a3
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-12 15:29:33 -0300

    commit

--- my-project/src/components/ui/input-opt.tsx
+++ my-project/src/components/ui/input-opt.tsx
@@ -12,9 +12,17 @@ const InputOTP = React.forwardRef<
         ref={ref}
         containerClassNAme={cn(
             "flex items-center gap-2 has-[:disabled] :opacity-50",
-            containerClassNAme={CN(
-                "flex items-center gap-2 has-[:disabled]:opacity-50",
-                
+          containerClassNAme         
             )}
-        )}
+        className={cn("disable:cursor-not-allowed", className)}
+        {...props}
+        />
 ))
+InputOTP.displayName = "InputOTP"
+
+const InputOTPGroup = React.forwardRef<
+    React.ElementRef<"div">,
+    React.ComponentPropsWithoutRef<"div">
+>(({ className, ...prpops }, ref) =>) (
+    <div ref={ref} className="{cn("flex items-center)}
+)
