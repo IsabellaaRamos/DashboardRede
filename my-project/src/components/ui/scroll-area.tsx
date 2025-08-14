b3326f1d1c2330c3e27c7ca89bab3615a6099c72
isabellaaRamos <isabellaramos7@outlook.com>
2025-08-14 09:12:30 -0300

    commit

--- my-project/src/components/ui/scroll-area.tsx
+++ my-project/src/components/ui/scroll-area.tsx
@@ -32,8 +32,10 @@ ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 const ScrollBar = React.forwardRef<
      React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
      (({ className, children, ...props }, ref) =>)(
-        
+        {...props}
      )
+     ScrollAreaPrimitive.displayName = ScrollAreaPrimitive.Root.displayName
+     
 
 
   
